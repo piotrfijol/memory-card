82c5668acc50b7f99d77fa8e92d1bda752e79dbf
Piotr Fijol <piotr.fijol1@gmail.com>
2021-05-26 23:44:01 +0200

    Coding app look, RWD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-
+import {useState} from 'react';
+import Playboard from './components/Playboard';
 
 function App() {
-  return (<div>
-    
+
+  const [name, setName] = useState("Cos");
+
+  function handleClick(e) {
+    const imie = prompt("Podaj swoje imie: ");
+    setName(imie);
+  } 
+
+  return (
+  <div className="container">
+    <header className="header">
+      <h1 className="header-title">Memory-card</h1>
+    </header>
+    <Playboard />
   </div>
   );
 }
---
+++ src/components/Playboard.js
@@ -0,0 +1,21 @@
+function Playboard() {
+
+    return (
+        <div className="playboard">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+        </div>
+    );
+}
+
+export default Playboard;
